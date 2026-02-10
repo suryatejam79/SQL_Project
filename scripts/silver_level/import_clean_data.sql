d3dc80b7e4bf2ec3c0cf9a6b84ddd8e3efe64e13
Surya Teja Mukka <mukkasuryateja@gmail.com>
2026-02-10 18:47:47 +0530

    Add SQL for importing cleaned product data
    Insert cleaned product information into silver.crm_prd_info from bronze.crm_prd_info with transformations.

--- scripts/silver_level/import_clean_data.sql
+++ scripts/silver_level/import_clean_data.sql
@@ -42,3 +42,30 @@ where cst_id is not null and rnk = 1;
 select * from silver.crm_cust_info;
 
 commit;
+
+
+-- this for next table I have quality check in another file name data_quality_testing_methods
+
+INSERT INTO silver.crm_prd_info
+(prd_id, cat_id, prd_key, prd_nm, prd_cost, prd_line, prd_start_dt, prd_end_dt)
+SELECT
+    prd_id,
+    REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+    SUBSTRING(prd_key, 7) AS prd_key,
+    prd_nm,
+    IFNULL(prd_cost, 0) AS prd_cost,
+    CASE
+        WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+        WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+        WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+        WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+        ELSE 'n/a'
+    END AS prd_line,
+    DATE(prd_start_dt) AS prd_start_dt,
+    DATE_SUB(
+        LEAD(prd_start_dt) OVER (PARTITION BY prd_nm ORDER BY prd_start_dt),
+        INTERVAL 1 DAY
+    ) AS prd_end_dt
+FROM bronze.crm_prd_info;
+
+commit;
