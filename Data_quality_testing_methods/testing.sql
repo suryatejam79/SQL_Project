36d6c58b89d55c50f98c461ef126ebf926a9bd95
Surya Teja Mukka <mukkasuryateja@gmail.com>
2026-02-10 18:49:14 +0530

    Implement data cleaning for crm_prd_info
    Added data cleaning queries for product information.

--- Data_quality_testing_methods/testing.sql
+++ Data_quality_testing_methods/testing.sql
@@ -55,4 +55,62 @@ when upper(trim(cst_gndr)) = 'F' then "Female"
 else "not_provided" end as cst_gndr from bronze.crm_cust_info;
 
 
+/* From here we working on second table */
+
+select * from bronze.crm_prd_info;
+-- First thing i will do in data cleaning i will go column by column so, I'm sure nothing is going to miss
+-- in the query the output first column is product id named as prd_id so, I have checked duplicates are there are not and shouldn't have null 
+-- so, for checks I have used below query
+
+select prd_id, count(*) from crm_prd_info
+group by prd_id
+having count(*) >1 or prd_id is null;
+
+-- there is no null and duplicate values so I don't want to disturb first column 
+-- Firstly while we clean the data we have to check is there any inter linking column for that or not if it common column we have to check which would be dim table and fact table
+/* AS I have checked second column is common column for this prd_info and prd_cat and sales table table and there is slightly different as it concat of two columns of two different 
+tables 
+So, While working on data we must check everything is there any inter linking between tables and what columns I must use for the analysis*/
+-- so, I must divide column and link to those two tables never forget to check spaces
+
+
+select replace(left(prd_key, 5),'-','_') as cat_id, substring(prd_key, 7, length(prd_key)) from crm_prd_info;
+
+-- here third column is prd_nm Here they can be duplicate but not null for as well can't have any spaces. so, i don't need any to check just can add trim
+
+select trim(prd_nm) as prd_nm from crm_prd_info;
+
+-- fourth column is prd_cost here i don't want any value should be in negative as there is no company in the world that gaves you product and money as  well
+
+select distinct prd_cost from crm_prd_info
+where prd_cost <0;
+
+-- there is no negative value if there is then i will use absolute 
+
+select abs(prd_cost) from crm_prd_info;
+
+-- Here 5th column I have information that don't use abbrevate names like shortcuts so, I have take insight of this column shortcut values
+
+select case
+when upper(trim(prd_line)) = 'M' then 'Mountain'
+when upper(trim(prd_line)) = 'S' then 'Other Sales'
+when upper(trim(prd_line)) = 'T' then 'Touring'
+when upper(trim(prd_line)) = 'R' then 'Road'
+else 'n/a'
+end as prd_line from crm_prd_info;
+
+-- here if we look 6th and 7th column they are date column and there not a chance that end date will come first and start date comes second
+-- here we can find the rows which there is typo error and we can change that
+
+select * from crm_prd_info
+where timestampdiff(day, prd_start_dt,prd_end_dt) <0 or prd_start_dt is null;
+
+-- and I don't need format in datetime I need only date datatype as I rarely use minutes and I know that i no need here
+-- so how to over come that there are many ways to over come and for me if i look prd_name repated as it is getting updated version there is mentioning it and creating prd_id 
+-- so, in we have partition by prd_nm and so, it will arrange and should be order by date in ascending so, upated product will come next and that date we can mark it as end date
+
+select date(prd_start_dt) as prd_start_dt, date_sub(date(lead(prd_start_dt) over(partition by prd_nm order by prd_start_dt)), interval 1 day) as prd_end_dt from crm_prd_info;
+
+
+
 
